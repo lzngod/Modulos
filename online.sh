288546b294a1c93fb1044ba196f5b7bddd5f813a
lzngod <48961438+lzngod@users.noreply.github.com>
2023-05-17 05:27:47 -0300

    Add files via upload

---
+++ online.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+    _ons=$(ps -x | grep sshd | grep -v root | grep priv | wc -l)
+    [[ -e /etc/openvpn/openvpn-status.log ]] && _onop=$(grep -c "10.8.0" /etc/openvpn/openvpn-status.log) || _onop="0"
+    [[ -e /etc/default/dropbear ]] && _drp=$(ps aux | grep dropbear | grep -v grep | wc -l) _ondrp=$(($_drp - 1)) || _ondrp="0"
+    _onli=$(($_ons + $_onop + $_ondrp))
+    _onlin=$(printf '%-5s' "$_onli")
+    echo $_onlin
+
+
+
